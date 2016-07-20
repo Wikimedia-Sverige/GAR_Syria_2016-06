7f9359c33aa5b40e90d1fb0298a50c99d309b1d6
Mattias Östmar <mattiasostmar@gmail.com>
2016-07-20 14:52:01 +0200

    Rerun 2 after fixes

--- create_metatdata_textfiles.py
+++ create_metatdata_textfiles.py
@@ -13,7 +13,6 @@ get_ipython().system('pip install pandas # A great library for data wrangling (a
 import pandas as pd 
 import regex
 import os 
-from polyglot.text import Text
 
 
 # # Metadata and mappings
@@ -27,23 +26,32 @@ from polyglot.text import Text
 # 
 # The doc below is downloaded locally 2016-07-19 16:55
 
-# In[3]:
+# In[75]:
 
-metadata_it = pd.read_excel("./data/COH_GAR_metadata.xlsx",sheetname="Italian", skiprows=[1]) # empty first row
-metadata_en = pd.read_excel("./data/COH_GAR_metadata.xlsx",sheetname="English")
+def strip(text):
+    try:
+        return text.strip()
+    except AttributeError:
+        return text
+    
+it_converters = {"Nome_foto":strip, "Anno":strip, "Luogo":strip, "Nome_monumento":strip, "Descrizione":strip, "Nome_autore":strip}
+metadata_it = pd.read_excel("./data/COH_GAR_metadata.xlsx",sheetname="Italian", skiprows=[1], converters=it_converters) # empty first row
+
+en_converters = {"Title":strip, "Year":strip, "Place": strip, "Subject":strip, "Description":strip, "Author":strip, "Commons_category":strip}
+metadata_en = pd.read_excel("./data/COH_GAR_metadata.xlsx",sheetname="English", converters=en_converters)
 
 
-# In[4]:
+# In[76]:
 
 metadata_it.head()
 
 
-# In[5]:
+# In[77]:
 
 metadata_en.head()
 
 
-# In[6]:
+# In[78]:
 
 merged = pd.concat([metadata_it,metadata_en], axis=1) 
 merged.head()
@@ -57,86 +65,84 @@ merged.head()
 
 # ## Places (Luogo)
 # 
-# We have two different mappings of places, one general based on the column "Place (Luogo)" in the original metadata file and one more specific which is a combination of the columns "Place (Luogo)" and "Subject (Nome monumento)". 
+# We have two different mappings of places, one general based on the column "Place (Luogo)" in the original metadata file and one more specific which is a combination of the columns "Place (Luogo)" and "Subject (Nome_monumento)". 
 # 
 # The mapping can be found here:
 # 
 # https://commons.wikimedia.org/wiki/Commons:Gruppo_Archeologico_Romano/Batch_upload/places
 
-# In[7]:
+# In[79]:
 
 place_mappings_url = "https://commons.wikimedia.org/wiki/Commons:Gruppo_Archeologico_Romano/Batch_upload/places"
 place_mappings = pd.read_html(place_mappings_url, attrs = {"class":"wikitable"}, header=0)
 # print(len(place_mappings))
 place_mappings_general = place_mappings[0]
+# Strip away potential surrounding whitespace
+place_mappings_general["Luogo"] = place_mappings_general.Luogo.str.strip() 
+place_mappings_general["wikidata"] = place_mappings_general.wikidata.str.strip()
+place_mappings_general["category"] = place_mappings_general.category.str.strip() 
+
 place_mappings_general = place_mappings_general.set_index("Luogo")
+
 place_mappings_specific = place_mappings[1]
-place_mappings_specific["Specific place"] = place_mappings_specific.Luogo + " " + place_mappings_specific["Nome monumento"]
-place_mappings_specific = place_mappings_specific[["Specific place" ,"Luogo","Nome monumento","category","wikidata"]]
-place_mappings_specific = place_mappings_specific.set_index("Specific place")
+# Strip away potential surrounding whitespace
+place_mappings_specific["Luogo"] = place_mappings_specific.Luogo.str.strip() 
+place_mappings_specific["Nome_monumento"] = place_mappings_specific.Nome_monumento.str.strip()
+place_mappings_specific["category"] = place_mappings_specific.category.str.strip()
+place_mappings_specific["wikidata"] = place_mappings_specific.wikidata.str.strip()
+
+place_mappings_specific["Specific_place"] = place_mappings_specific.Luogo + " " + place_mappings_specific.Nome_monumento
+place_mappings_specific = place_mappings_specific[["Specific_place" ,"Luogo","Nome_monumento","category","wikidata"]]
+place_mappings_specific = place_mappings_specific.set_index("Specific_place")
 
 
-# In[8]:
+# In[80]:
+
+place_mappings_general.category.str.strip() 
+
+
+# In[81]:
 
 print(place_mappings_general.head(10))
 
 
-# In[9]:
+# In[82]:
 
 print(place_mappings_specific.head(10))
 
 
-# # Population of {{Photograph}}
+# # Population of the Photograph template
 # 
 # ## Template mapping
 
-# In[16]:
+#  The master template mapping lives as [a task on Phabricator](https://phabricator.wikimedia.org/T139724)
 
-desc = "Rovine del tempio di Giove all'ingresso del"
-text = Text(desc)
+# ## Collect original filenames
+
+# In[45]:
+
+sub_dirs = []
+original_filenames = []
+
+root_dir = "/Users/mos/GAR pictures for WMSE/"
+for root, dirs, files in os.walk("/Users/mos/GAR pictures for WMSE/"):
+    sub_dirs.extend(dirs)
+for dir in sub_dirs:
+    for root, dirs, files in os.walk(root_dir + dir):
+        for file in files:
+            if file.endswith(".JPG") or file.endswith(".jpg"):
+                original_filenames.append(file)
+print("Subdirs: {}\nlen(original_filenames): {}\nFiles in metadata file: 426".format(len(sub_dirs), len(original_filenames)))
 
 
-# In[22]:
-
-lang = text.language
-
-
-# In[23]:
-
-lang.code
-
- |photographer       = <en-Author[4:]>
- |title              =  
-{{it|’’’<Nome foto[:-3]>’’’}}
-{{en|<Title[:-3]>}}
- |description        =
-{{en|<Description> OR <Subject>, <Place> in <year>}}
-{{it|<Descrizione> OR <Nome monumento>, <Luogo>, <Anno>}} 
- |depicted people    = 
- |depicted place     =  {{city|<wikidata from Specific Place mapping> OR <wikidata from Luogo (place) mapping>}} OR <Subject, Place>
- |date               = <Year>
- |medium             = 
- |dimensions         =
- |institution        = {{Institution:Gruppo Archeologico Romano}}
- |department         =
- |references         =
- |object history     =
- |exhibition history =
- |credit line        =
- |inscriptions       =
- |notes              =
- |accession number   =
- |source             = {{Gruppo Archeologico Romano cooperation project|COH}}
- |permission         = {{CC-BY-SA-4.0|<en-Author[4:]> / ”GAR”}}
-{{PermissionOTRS|id=2016042410005958}}
- |other_versions     =
-}}
-# ## Code
+# ## Create wikitext for image pages
 # Available as .py script on [my github](https://github.com/mattiasostmar/GAR_Syria_2016-06/blob/master/create_metatdata_textfiles.py)
 
-# In[24]:
+# In[87]:
 
+# remove possible diuplicate files with other extension names
 get_ipython().system('rm -rf ./photograph_template_texts/*')
+
 total_images = 0
 OK_images = 0
 uncategorized_images = 0
@@ -150,59 +156,45 @@ for row_no, row in merged.iterrows():
     header = "{{Photograph"
     template_parts.append(header)
     
-    if not pd.isnull(row["Author"]):
+    if not pd.isnull(row["Author"]): 
         photographer = "|photographer = " + row["Author"][4:]
     else:
-        print("Warning! Empty Author column in row no {} photo {}".format(row_no, row["Nome foto"]))
+        print("Warning! Empty Author column in row no {} photo {}".format(row_no, row["Nome_foto"]))
         photographer = "|photographer = "
         faulty_images += 1
     template_parts.append(photographer)
     
-    title_it =  "{{it|'''" + regex.sub("_"," ",row["Nome foto"][:-3]) + "'''}}"
-    title_en = "{{en|'''" + regex.sub("_"," ", row["Title"][:-3]) + "'''}}"
+    title_it =  "{{it|'''" + regex.sub("_"," ",row["Nome_foto"][:-3]) + "'''}}"
+    title_en = "{{en|" + regex.sub("_"," ", row["Title"][:-3]) + "}}"
     title = "|title = " + title_it + title_en
     template_parts.append(title)
     
     if pd.notnull(row["Description"]):
-        text = Text(row["Description"])
-        lang = text.language
-        
-        if lang.code != "en":
-            print("name {} text {} lang code {} - expected 'en'".format(row["Title"], text, lang.code))
-        else:
-            if len(row["Description"].split()) >= 3:
-                description_en = "{{en| = " + row["Description"] + "}}"
-            else:
-                description_en = "{{en| = " + str(row["Subject"]) + " " + str(row["Place"]) + " in " + str(row["Year"]) + "}}"
+        description_en = "{{en| " + row["Description"] + "}}"
     else:
-        if lang.code != "en":
-            print("name {} text {} lang code {} - expected 'en'".format(row["Title"], text, lang.code))
-        else:    
-            description_en = "{{en| = " + str(row["Subject"]) + " " + str(row["Place"]) + " in " + str(row["Year"]) + "}}"
+        description_en = "{{en| = " + str(row["Subject"]) + ", " + str(row["Place"]) + " in " + str(row["Year"]) + "}}"
     
     if pd.notnull(row["Descrizione"]):
-        if len(row["Descrizione"].split()) >= 3:
-            description_it = "{{it| = " + row["Descrizione"] + "}}"
-        else:
-            description_it = "{{it| = " + str(row["Nome monumento"]) + " " + str(row["Luogo"]) + " " + str(row["Anno"]) + "}}"
+        description_it = "{{it| = " + row["Descrizione"] + "}}"
     else:
-        description_it = "{{it| = " + str(row["Nome monumento"]) + " " + str(row["Luogo"]) + " " + str(row["Anno"]) + "}}"
+        description_it = "{{it| " + str(row["Nome_monumento"]) + ", " + str(row["Luogo"]) + ", " + str(row["Anno"]) + "}}"
     
-    description = "|description = " + description_it + description_en
+    description = "|description = " + description_it + "\n" + description_en
     template_parts.append(description)
     
     depicted_people = "|depicted people ="
     template_parts.append(depicted_people)
     
-    if pd.notnull(place_mappings_specific.loc[row["Luogo"] + " " + row["Nome monumento"]]["wikidata"]):
-        depicted_place = "|depicted place = {{city|" +         place_mappings_specific.loc[row["Luogo"] + " " + row["Nome monumento"]]["wikidata"] + "}}"
+    if pd.notnull(place_mappings_specific.loc[row["Luogo"] + " " + row["Nome_monumento"]]["wikidata"])     or place_mappings_specific.loc[row["Luogo"] + " " + row["Nome_monumento"]]["wikidata"] == "-":
+        depicted_place = "|depicted place = {{city|" +         place_mappings_specific.loc[row["Luogo"] + " " + row["Nome_monumento"]]["wikidata"] + "}}"
         #print(depicted_place)
-    elif pd.notnull(place_mappings_general.loc[row["Luogo"]]["wikidata"]):
+    elif pd.notnull(place_mappings_general.loc[row["Luogo"]]["wikidata"])     or place_mappings_general.loc[row["Luogo"]]["wikidata"] == "-":
         depicted_place = "|depicted place = {{city|" +         place_mappings_general.loc[row["Luogo"]]["wikidata"] + "}}"
         #print(depicted_place)
     else:
-        depicted_place = "|depicted place = " + row["Luogo"] + " " + row["Nome monumento"]
+        depicted_place = "|depicted place = " + row["Luogo"] + " " + row["Nome_monumento"]
         #print(depicted_place)
+    template_parts.append(depicted_place)
     
     if pd.notnull(row["Year"]):
         date = "|date = " + str(row["Year"])
@@ -243,13 +235,13 @@ for row_no, row in merged.iterrows():
     accession_number = "|accession number ="
     template_parts.append(accession_number)
     
-    source = "|source = {{Gruppo Archeologico Romano cooperation project|COH}}"
+    source = "|source = " + row["Nome_foto"] + "\n{{Gruppo Archeologico Romano cooperation project|COH}}"
     template_parts.append(source)
     
     if pd.notnull(row["Author"]):
-        permission = "|permission = {{CC-BY-SA-4.0|" + row["Author"][4:] + "|GAR}}{{PermissionOTRS|id=2016042410005958}}"
+        permission = "|permission = {{CC-BY-SA-4.0|" + row["Author"][4:] + "/ GAR}}\n{{PermissionOTRS|id=2016042410005958}}"
     else:
-        permission = "|permission = {{CC-BY-SA-4.0|Gruppo Archeologico Romano}}{{PermissionOTRS|id=2016042410005958}}"
+        permission = "|permission = {{CC-BY-SA-4.0|Gruppo Archeologico Romano}}\n{{PermissionOTRS|id=2016042410005958}}"
     template_parts.append(permission)
     
     other_versions = "|other_versions ="
@@ -265,8 +257,8 @@ for row_no, row in merged.iterrows():
     maintanence_category = None
     batchupload_category = "[[Category:Images_from_GAR_2016-06]]"
     
-    if pd.notnull(place_mappings_specific.loc[row["Luogo"] + " " + row["Nome monumento"]]["category"]):
-        specific_place_category = "[[" + place_mappings_specific.loc[row["Luogo"] + " " + row["Nome monumento"]]["category"] + "]]"
+    if pd.notnull(place_mappings_specific.loc[row["Luogo"] + " " + row["Nome_monumento"]]["category"]):
+        specific_place_category = "[[" + place_mappings_specific.loc[row["Luogo"] + " " + row["Nome_monumento"]]["category"] + "]]"
         #print(specific_place_category)
     
     elif pd.notnull(place_mappings_general.loc[row["Luogo"]]["category"]):
@@ -289,12 +281,14 @@ for row_no, row in merged.iterrows():
         categories_list.append(maintanence_category)
 
     categories_list.append(batchupload_category)
-
+        
     #print(categories_list)
     
-    # Filename: <Nome foto[:-3]>_GAR_<Nome foto[-3:]>.<ext>
+    # Filename: <Nome_foto[:-3]>_GAR_<Nome_foto[-3:]>.<ext>
     outpath = "./photograph_template_texts/"
-    fname = row["Nome foto"][:-3] + " - " + "GAR" + " - " + row["Nome foto"][-3:] + ".JPG" # Hack, extension ought to be dynamic
+    fname = row["Nome_foto"][:-3] + " - " + "GAR" + " - " + row["Nome_foto"][-3:] + ".JPG" # Hack, extension ought to be dynamic
+    
+    
     if not os.path.exists(outpath):
         os.mkdir(outpath)
         outfile = open(outpath + fname, "w")
